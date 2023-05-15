aa4321c9b5b4b403683e418c9508407d7621cac4
Aarón <132054915+arondx@users.noreply.github.com>
2023-05-15 17:07:08 +0100

    Add Topbar, SideNavbar and Dashboard components

---
+++ src/components/sideNavbar/SideNavbar.jsx
@@ -0,0 +1,84 @@
+export default function SideNavbar() {
+    return (
+        <>
+            <div id="sideNavbar" className="w-80 h-screen bg-zinc-100">
+                
+                <div className="flex justify-between p-4">
+                
+                    <figure>
+                        <img className="h-16 w-16 rounded-full drop-shadow-lg" src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas_23-2149436190.jpg?w=826&t=st=1684150294~exp=1684150894~hmac=969b76a5e6c799718cee916665a270be3985d9f8cc323864e2d36e7b73859e34" alt="logo" />
+                    </figure>
+
+                    <div className="flex gap-2 flex-col justify-end">
+
+                        <span>Welcome, <strong>Mike</strong></span>
+
+                        <div className="flex justify-between gap-2">
+
+                            <a className="h-8 w-8 flex items-center justify-center hover:bg-zinc-300" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+
+                            <a className="h-8 w-8 flex items-center justify-center hover:bg-zinc-300" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+                                    <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                                </svg>
+                            </a>
+
+                            <a className="h-8 w-8 flex items-center justify-center hover:bg-zinc-300" href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clip-rule="evenodd" />
+                                </svg>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <hr />
+                <div>
+                
+                    <h2 className=" text-xl font-bold px-4 py-4">Dashboard</h2>
+                    <div>
+                        
+                        <a href="" className="flex px-4 py-4 gap-2 bg-zinc-600 text-white hover:bg-zinc-950">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            <span>Close Menu</span>
+                        </a>
+
+                        <a href="" className="flex px-4 py-2 gap-2 hover:bg-zinc-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                            </svg>
+                            <span>Users</span>
+                        </a>
+
+                        <a href="" className="flex px-4 py-2 gap-2 hover:bg-zinc-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                            </svg>
+                            <span>Bookings</span>
+                        </a>
+
+                        <a href="" className="flex px-4 py-2 gap-2 hover:bg-zinc-300 ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 13.5H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                            </svg>
+                            <span>History</span>
+                        </a>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div id="sideNavbar-overlay" className="absolute -z-50 opacity-50 h-screen inset-0 bg-zinc-500"></div>
+        </>
+    )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,9 @@
+import './topbar.css';
+
+export default function Topbar() {
+  return (
+    <div id="topbar" className='flex justify-end items-center h-14 bg-zinc-950 text-white'>
+        <span className='px-4'>Logo</span>
+    </div>
+  )
+}
